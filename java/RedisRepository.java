7e6369d47171801562f9162047efef62e2d7b252
hjkim27 <88189591+hjkim27@users.noreply.github.com>
2024-08-08 21:59:31 +0900

    Create RedisRepository.java

---
+++ java/RedisRepository.java
@@ -0,0 +1,250 @@
+package kr.co.privacycenter.mapper;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.data.redis.connection.DataType;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.SetOperations;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.stereotype.Service;
+
+import kr.co.privacycenter.exception.RedisException;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+redis 연동 샘플코드
+*/
+@Slf4j
+@Service
+public class RedisRepository {
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    private static StringRedisTemplate stringRedisTemplate2;
+    
+    public RedisRepository(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+        this.stringObjectObjectHashOperations = stringRedisTemplate.opsForHash();
+        this.stringStringListOperations = stringRedisTemplate.opsForList();
+        this.stringStringZSetOperations = stringRedisTemplate.opsForZSet();
+        this.stringStringSetOperations = stringRedisTemplate.opsForSet();
+        this.stringRedisTemplate2 = stringRedisTemplate;
+    }
+
+    private final HashOperations<String, Object, Object> stringObjectObjectHashOperations;
+    private final ListOperations<String, String> stringStringListOperations;
+    private final ZSetOperations<String, String> stringStringZSetOperations;
+    private final SetOperations<String, String> stringStringSetOperations;
+
+    public static String getPrivateDBName(String domain) throws RedisException {
+    	try {
+            String value = stringRedisTemplate2.opsForValue().get("db/"+domain);
+            return value;
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+    
+    public void set(String key, String value, long expire) throws RedisException {
+        try {
+            stringRedisTemplate.opsForValue().set(key, value, expire, TimeUnit.MILLISECONDS);
+        } catch (Exception e) {
+            throw e;
+        }    }
+    
+    public void set(String key, String value) throws RedisException {
+    	try {
+    		stringRedisTemplate.opsForValue().set(key, value);
+    	} catch (Exception e) {
+    		throw e;
+    	}
+    }
+
+    public String get(String key) throws RedisException {
+        try {
+            String value = stringRedisTemplate.opsForValue().get(key);
+            return value;
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public Boolean expireAt(String key, Date date) throws RedisException {
+        try {
+            return stringRedisTemplate.expireAt(key, date);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public long deleteByKey(String key) throws RedisException {
+        try {
+            if (stringRedisTemplate.delete(key)) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public long deleteByKeys(Collection<String> keys) throws RedisException {
+        try {
+            return stringRedisTemplate.delete(keys);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public long delete(Collection<String> keys) throws RedisException {
+        try {
+            return stringRedisTemplate.delete(keys);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public Set<String> getPatternKey(String pattern) throws RedisException {
+        try {
+            return stringRedisTemplate.keys(pattern);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public void renameKey(String oldKey, String newKey) throws RedisException {
+        try {
+            stringRedisTemplate.rename(oldKey, newKey);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public DataType getKeyType(String key) throws RedisException {
+        try {
+            return stringRedisTemplate.type(key);
+        } catch (Exception e) {
+            throw e;
+        }
+
+    }
+
+    public boolean setFromMap(Map<String, String> map) throws RedisException {
+        try {
+            return Boolean.TRUE.equals(stringRedisTemplate.opsForValue().multiSetIfAbsent(map));
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public Map<Object, Object> hGetAll(String key) throws RedisException {
+        try {
+            return stringObjectObjectHashOperations.entries(key);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public void hPutAll(String key, Map<String, String> maps) throws RedisException {
+        try {
+            stringObjectObjectHashOperations.putAll(key, maps);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public void hPut(String key, String hashKey, String value) throws RedisException {
+        try {
+            stringObjectObjectHashOperations.put(key, hashKey, value);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public boolean hashExists(String key, String field) throws RedisException {
+        try {
+            return stringObjectObjectHashOperations.hasKey(key, field);
+        } catch (Exception e) {
+            throw e;
+        }
+
+    }
+
+    public Set<Object> getHashFields(String key) throws RedisException {
+        try {
+            return stringObjectObjectHashOperations.keys(key);
+        } catch (Exception e) {
+            throw e;
+        }
+
+    }
+
+    public List<Object> hValues(String key) throws RedisException {
+        try {
+            return stringObjectObjectHashOperations.values(key);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+    
+    public void delInbound(String domain, int port) throws RedisException {
+    	log.debug("delete policy/inbound/"+domain +"\t, dupl/inbound/"+domain);
+    	deleteByKey("policy/inbound/"+domain);
+    	deleteByKey("dupl/inbound/"+domain);
+    	if(port != -1) {
+    		log.debug("delete policy/inbound/"+domain+":"+port +"\t, dupl/inbound/"+domain+":"+port);
+    		deleteByKey("policy/inbound/"+domain+":"+port);
+    		deleteByKey("dupl/inbound/"+domain+":"+port);
+    	}
+    	
+    }
+    public void delOutbound(String domain, int port) throws RedisException {
+    	log.debug("delete policy/outbound/"+domain +"\t, dupl/outbound/"+domain);
+    	deleteByKey("policy/outbound/"+domain);
+    	if(port != -1) {
+    		log.debug("delete policy/outbound/"+domain+":"+port +"\t, dupl/outbound/"+domain+":"+port);
+    		deleteByKey("policy/outbound/"+domain+":"+port);
+    	}
+    }
+    
+    public void setInbound(String domain) throws RedisException {
+    	log.debug("set policy/inbound/"+domain);
+    	set("policy/inbound/"+domain, domain);
+    }
+    public void setOutbound(String domain) throws RedisException {
+    	log.debug("set policy/outbound/"+domain);
+    	set("policy/outbound/"+domain, domain);
+    }
+    
+    // hjkim [2022.10.14] 필터링웹서버 추가/수정 시 "db/domain:port" 를 set/del 하도록 추가 
+    public void setFilteringServerDomain(String domain, Integer port, Integer id) throws RedisException {
+    	log.debug("set db/"+domain);
+    	set(String.valueOf("db/"+domain), String.valueOf("privacycenter_"+id));
+    	if(port!= -1) {
+    		log.debug("set db/"+domain+":"+port);
+    		set(String.valueOf("db/"+domain+":"+port), String.valueOf("privacycenter_"+id));    	
+    	}
+//    	set(String.valueOf("db/"+domain), "privacycenter_master");
+//    	set(String.valueOf("db/"+domain+":"+port), "privacycenter_master");
+    }
+    
+    public void delFilteringServerDomain(String domain, Integer port, boolean deleteCheck) throws RedisException {
+    	if(deleteCheck) {
+    		log.debug("delete db/"+domain);
+    		deleteByKey("db/"+domain);
+    	}
+    	if(port!= -1) {
+    		log.debug("delete db/"+domain+":"+port);
+    		deleteByKey("db/"+domain+":"+port);
+    	}
+    }
+    
+}
